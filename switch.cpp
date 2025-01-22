47e60084e271d7542b95371d449ecaf1049641fa
RAJBIR BISWAS <146480609+ELiTE0005@users.noreply.github.com>
2025-01-22 10:21:41 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std ;
+
+int main () {
+
+   int i;
+
+   cout<<"enter the i : ";
+   cin>>i;
+
+   switch (i) {
+
+    case 1 :
+    cout<<"raj";
+    break;
+    
+    case 2 :
+    cout<<"rj1";
+    break;
+
+    default :
+    cout<<"rj2";
+    
+   }
+}
