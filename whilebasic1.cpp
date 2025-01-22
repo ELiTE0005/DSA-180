89168a61e0f4a0a1181aa25dcddb11056233c15e
RAJBIR BISWAS <146480609+ELiTE0005@users.noreply.github.com>
2025-01-22 09:47:56 +0530

    Add files via upload

---
+++ whilebasic1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n , i=1 ;
+
+    cout<<"enter the number: ";
+    cin>>n;
+
+    while ( i<=n ) {
+
+        if (n%i == 0) {
+
+            cout<<i<<" ";
+        }
+
+        i++;
+    }
+
+    return 0;
+}
+
+
